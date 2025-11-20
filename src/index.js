f634f15dc7cb8d3379c4b4ea9217863acbe7fdb4
InnocentSinner08 <innocentsinner08114@gmail.com>
2025-11-20 14:54:07 +0530

    exporting app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv"
-// require('dotenv').config({path: './env'})
+
 dotenv.config({
     path: './env'
 })
+
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server listening to ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection Error: ", error);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// require('dotenv').config({path: './env'})
 // Method 2
 // const app= express()
 // ( async ()=>{
