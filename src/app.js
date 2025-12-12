6102208f08c8c1098428696bcd87cf4d11bc6a5e
sheetalrajr <sheetalraj004@gmail.com>
2025-12-12 21:52:16 +0530

    Check

--- src/app.js
+++ src/app.js
@@ -12,4 +12,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+// Router
+import userRoutes from './routes/user.routes.js'
+
+app.use("/users", userRoutes)
 export {app}
